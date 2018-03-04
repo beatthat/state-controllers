c703f8be2beba5b86101aa45e0478bfee8619813
larrykirschner <larrykirschner@gmail.com>
2018-03-04 14:13:52 -0800

    missed overload functions for Param::DefaultParamNameXXX in prev copy from unity

--- src/state-controllers/Param.cs
+++ src/state-controllers/Param.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 
 namespace BeatThat
@@ -9,14 +10,49 @@ namespace BeatThat
 	public interface Param 
 	{
 		string param { get; }
+
+		Type paramType { get; }
 	}
 
 	public static class ParamExt
 	{
 		public static string DefaultParamName(this Param p)
 		{
-			var t = p.GetType().Name;
+			return DefaultParamName (p.GetType ());
+		}
+
+		public static string DefaultParamName<T>() where T : Param
+		{
+			return DefaultParamName (typeof(T));
+		}
+
+		public static string DefaultParamName(Type type)
+		{
+			var t = type.Name;
 			return Char.ToLower(t[0]) + t.Substring(1);
+		}
+
+
+		public static string DefaultParamNameRemovingSuffixes(this Param p, params string[] suffixes)
+		{
+			return DefaultParamNameRemovingSuffixes (p.GetType (), suffixes);
+		}
+
+		public static string DefaultParamNameRemovingSuffixes<T>(params string[] suffixes)
+		{
+			return DefaultParamNameRemovingSuffixes (typeof(T), suffixes);
+		}
+
+		public static string DefaultParamNameRemovingSuffixes(Type type, params string[] suffixes)
+		{
+			var t = type.Name;
+			var n = Char.ToLower(t[0]) + t.Substring(1);
+			foreach(var s in suffixes) { 
+				if (n.EndsWith (s)) {
+					n = n.Substring (0, n.Length - s.Length);
+				}
+			}
+			return n;
 		}
 	}
 }
